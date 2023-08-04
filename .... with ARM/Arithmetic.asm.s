91a9864a545001a744a077ea041f8368174dbbc8
Fushiguro19 <86782830+Fushiguro19@users.noreply.github.com>
2023-08-04 20:08:47 +0530

    Update Arithmetic.asm.s

--- .... with ARM/Arithmetic.asm.s
+++ .... with ARM/Arithmetic.asm.s
@@ -1,7 +1,7 @@
-/* the SUB gives a neg value and we know it from the N flag set in the "NZCVI" flags section of the CPSR register. To set the flag in 
- it, we need to add an "S" after SUB instruction. "S" is also added when u don't knw what values r u gng to SUB.
- The "C" flag is set in CPSR when there is an overflow : resultant of an ADD operation is more than 32 bits (there is a carry over)
- To include the carry : ADC R2,R0,R1 // R2 = R0+R1+carry ... carry will be 1 if flag is set to 1 */
+ //the SUB gives a neg value and we know it from the N flag set in the "NZCVI" flags section of the CPSR register. To set the flag in 
+ #it, we need to add an "S" after SUB instruction. "S" is also added when u don't knw what values r u gng to SUB.
+ @The "C" flag is set in CPSR when there is an overflow : resultant of an ADD operation is more than 32 bits (there is a carry over)
+ To include the carry : ADC R2,R0,R1 // R2 = R0+R1+carry ... carry will be 1 if flag is set to 1 
 
 .global _stack
 _stack:
