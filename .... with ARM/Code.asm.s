1f2c3f9f7e8b750543c4b1c5dcec54422e4431dd
Fushiguro19 <86782830+Fushiguro19@users.noreply.github.com>
2023-08-04 20:14:07 +0530

    Update Code.asm.s

--- .... with ARM/Code.asm.s
+++ .... with ARM/Code.asm.s
@@ -1,11 +1,11 @@
-#_start is a "label" :  like functions in high-level prog lang ........ When u go to a label, code under it is executed
-# .global makes the _start label accessible to entities outside our program
-# In this program we will place some numbers in register r0 and then move to r7 -> place some number there and call system interrupt
-# ... to interrupt the OS and as it to do something for us... the OS then reads the number in r7 and looks for a task corresponding to that number
-# ... and performs it for us
-# Here we will place number 1 in r7 which tells the OS to exit/end the program
-# MOV Destination,Source : Move data from Source to Destination ... here our source is a constant so we use # and destination is register r0
-# SWI : SoftWare Interrupt ..... SWI 0 means an interrupt to make the OS perform the exit program task
+;_start is a "label" :  like functions in high-level prog lang ........ When u go to a label, code under it is executed
+; .global makes the _start label accessible to entities outside our program
+; In this program we will place some numbers in register r0 and then move to r7 -> place some number there and call system interrupt
+; ... to interrupt the OS and as it to do something for us... the OS then reads the number in r7 and looks for a task corresponding to that number
+; ... and performs it for us
+; Here we will place number 1 in r7 which tells the OS to exit/end the program
+; MOV Destination,Source : Move data from Source to Destination ... here our source is a constant so we use # and destination is register r0
+; SWI : SoftWare Interrupt ..... SWI 0 means an interrupt to make the OS perform the exit program task
 
 .global _start
 _start: 
