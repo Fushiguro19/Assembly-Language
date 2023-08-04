088f0bb34ae0938b17deee038045960edaf528e4
Fushiguro19 <86782830+Fushiguro19@users.noreply.github.com>
2023-08-04 20:13:10 +0530

    Update Conditionals.asm.s

--- .... with ARM/Conditionals.asm.s
+++ .... with ARM/Conditionals.asm.s
@@ -8,12 +8,12 @@ _start:
   MOV R1,#5
 
   CMP R0,R1     
-  #It will compute R0-R1 and based on the result it will decide the relation between the 2 which will set the CPSR flag accordinghly
-  #say if R0-R1 = neg number then CPSR flag N will be set indicating a neg ans and that R0 is smaller than R1
+  ;It will compute R0-R1 and based on the result it will decide the relation between the 2 which will set the CPSR flag accordinghly
+  ;say if R0-R1 = neg number then CPSR flag N will be set indicating a neg ans and that R0 is smaller than R1
   BGT greater     
-  # B : Branch     GT : Greater Than      greater : A label whose code to execute if R0 > R1 
+  ; B : Branch     GT : Greater Than      greater : A label whose code to execute if R0 > R1 
   
-  MOV R2,#2     # If R0 is not > R1 then this line will be executed and BGT will be skipped
+  MOV R2,#2     ; If R0 is not > R1 then this line will be executed and BGT will be skipped
 
 greater:
   MOV R2, #1
